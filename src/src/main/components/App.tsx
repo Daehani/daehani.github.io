59d1cc095ed802807a6ac4bd69f700245de3a4ce
daehan.park <daehan.park@navercorp.com>
2024-07-19 16:54:26 +0900

    a

--- src/src/main/components/App.tsx
+++ src/src/main/components/App.tsx
@@ -8,13 +8,13 @@ import Toast, { ToastLevel } from './Toast';
 declare const naver: any;
 
 function App() {
-  const quoteText = "여름의 첫 만남, 가을의 새로운 시작\n" +
+  const inviteText = "여름의 첫 만남, 가을의 새로운 시작\n" +
     "서로를 통해 나를 발견하는\n" +
     "우리만의 여행을 시작하려 합니다.\n" +
     "\n" +
     "이 특별한 순간에 함께해 주실\n" +
     "고마운 분들을 초대합니다.";
-  const quoteReference = "2024년 10월 6일 일요일 오후 1시\n" +
+  const inviteText2 = "2024년 10월 6일 일요일 오후 1시\n" +
     "용산 가족 공원";
   const informations = [
     ["자가용 안내", ["국립중앙박물관 지하주차장 이용", "소액의 주차비용 발생, 양해 부탁드립니다.\n 최초 2시간 2000원, 초과 30분당 500원"]],
@@ -22,6 +22,9 @@ function App() {
     ["피로연 안내", ["국립중앙박물관 거울못식장 오후 2시"]],
     ["화환 반입", ["공원 운영정책에 따라 정중히 사양합니다."]]
   ];
+  const tailText = "저희 결혼을 축하해 주셔서 정말 감사합니다.\n" +
+    "가을 날 편안한 차림으로 함께 즐겨요."
+              
 
   const photos = [...new Array(34).keys()].map(it => (it + 1).toString().padStart(3, "0")).filter(it => !["006", "011"].includes(it));
   // const photosContinuable: Array<string> = [];
@@ -104,43 +107,29 @@ function App() {
 
         {/* 대문사진 */}
         <div className="intro">
-          {/* <div className="overlay" /> */}
-          {/* <div className="border" /> */}
+          <div className="overlay" />
+          <div className="border" />
           <img src={introImageUrl} className="image" />
         </div>
 
         <div className="context">
-
-          {/* 장소/일시 */}
-          {/* <div className="abstract">
-            <div className="who">Seokmo and Jiyul</div>
-            <div className="when">2024년 6월 22일 토요일 오전 11시 30분</div>
-            <div className="where">충무로 라비두스</div>
-          </div> */}
-
-          {/* 초대글 */}
-          <p className="category font-script">
-            대한과 지수 결혼합니다.
-          </p>          
-          <div className="quote">
-            {quoteText.split("\n").map((it, index) => <p key={index} className="contents">{it}</p>)}
-            <p className="contents">{quoteReference}</p>
+          {/* 초대글 */}       
+          <div className="invitation">
+            <p className="category font-script">
+              대한과 지수 결혼합니다.
+            </p>   
+            {inviteText.split("\n").map((it, index) => <p key={index} className="contents">{it.trim() === '' ? <span>&nbsp;</span> : it}</p>)}
+            {inviteText2.split("\n").map((it, index) => <p key={index} className="contents">{it.trim() === '' ? <span>&nbsp;</span> : it}</p>)}
           </div>
 
-
-          {/* 중간 사진 */}
-          {/* <div className="bridge-image">
-            <img src={bridgeImageUrl}/>
-          </div> */}
 
           {/* 지도 */}
           <div className="location">
             <p className="category font-script">
-              용산가족공원 연못 옆\n 버드나무 아래
+              용산가족공원 연못 옆 버드나무 아래
             </p>
-            <p className="contents">
-              서울 용산구 서빙고로 137\n 국립중앙박물관 옆 용산가족공원
-            </p>
+            <p className="contents"> 서울 용산구 서빙고로 137</p>
+            <p className="contents"> 국립중앙박물관 옆 용산가족공원</p>
           </div>
           <Map
             assetsBaseUrl={assetsBaseUrl}
@@ -172,11 +161,7 @@ function App() {
           
           
           <div className="tail">
-            <p className="contents">
-              저희 결혼을 축하해 주셔서 정말 감사합니다.
-              가을 날 편안한 차림으로 함께 즐겨요.
-            </p>
-            {quoteText.split("\n").map((it, index) => <p key={index} className="contents">{it}</p>)}
+            {tailText.split("\n").map((it, index) => <p key={index} className="contents">{it}</p>)}
           </div>
 
         </div>
