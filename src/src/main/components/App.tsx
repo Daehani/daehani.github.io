8f1bc57362c4a13fbe5a73d042521537ed30a0f0
daehan.park <daehan.park@navercorp.com>
2024-08-18 14:46:43 +0900

    a

--- src/src/main/components/App.tsx
+++ src/src/main/components/App.tsx
@@ -113,9 +113,10 @@ function App() {
           <p className="contents"> <span>&nbsp;</span> </p>
           <p className="contents"> <span>&nbsp;</span> </p>
           {locationText.split("\n").map((it, index) => <p key={index} className="contents1">{it.trim() === '' ? <span>&nbsp;</span> : it}</p>)}
-          <p className="space1"> <span>&nbsp;</span> </p>
+          <p className="contents"> <span>&nbsp;</span> </p>
           {locationText2.split("\n").map((it, index) => <p key={index} className="contents2">{it.trim() === '' ? <span>&nbsp;</span> : it}</p>)}
           {locationText3.split("\n").map((it, index) => <p key={index} className="contents3">{it.trim() === '' ? <span>&nbsp;</span> : it}</p>)}
+          <p className="contents"> <span>&nbsp;</span> </p>
           <p className="overlay1">
             <Map assetsBaseUrl={assetsBaseUrl}/>
           </p>
